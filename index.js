5ec11dab3684cc4e242a2a4c0b8ec54e0835bedc
vishnu-prasath-M <imvishnuprasath@gmail.com>
2024-10-28 21:27:39 +0530

    Added javascript

---
+++ index.js
@@ -0,0 +1,35 @@
+var menuicon=document.getElementById("menuicon")
+var sidenav=document.getElementById("sidenav")
+var closenav=document.getElementById("closenav")
+
+menuicon.addEventListener("click",function(){
+    sidenav.style.right=0
+})
+
+closenav.addEventListener("click",function(){
+    sidenav.style.right="-50%"
+})
+
+
+
+
+var productContainer = document.getElementById("product-container")
+var search = document.getElementById("search")
+var productlist = productContainer.querySelectorAll("div")
+
+    search.addEventListener("keyup",function(){
+        var enteredValue = event.target.value.toUpperCase()
+        
+        for(count = 0; count<productlist.length; count=count+1)
+        {
+            var productname = productlist[count].querySelector("h1").textContent
+
+            if(productname.toUpperCase().indexOf(enteredValue)<0)
+            {
+                productlist[count].style.display="none"
+            }
+            else{
+                productlist[count].style.display="block"
+            }
+        }
+    })
